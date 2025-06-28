73b6a5aac6d80382a6cfb156eda61392389d9bdd
unknown <neel26ray@gmail.com>
2025-06-29 00:31:07 +0530

    feat : shop and admin schema , basic folder structure for fe and be

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-100 text-gray-800'>
+    App</div>
+  )
+}
+
+export default App
